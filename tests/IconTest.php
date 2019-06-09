64c30894d5fafdb2c63f16df3f5cfdf87c3003f2
Thoulah <johan@mr42.me>
2019-06-09 16:44:21 +0200

    Ensure working PHPUnit environment

---
+++ tests/IconTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Thoulah\FontAwesomeInline\tests;
+
+/**
+ * @group FontAwesomeInline
+ */
+class IconTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTrueAssertsToTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
