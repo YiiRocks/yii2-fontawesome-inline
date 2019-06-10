718a0b9847526caec034c9e6fff2a20f2cdf1c54
Thoulah <johan@mr42.me>
2019-06-10 04:04:02 +0200

    Description update

--- tests/ActiveFormTest.php
+++ tests/ActiveFormTest.php
@@ -4,7 +4,6 @@ namespace Thoulah\FontAwesomeInline\tests;
 use Yii;
 use Thoulah\FontAwesomeInline\bootstrap4\ActiveForm;
 use yii\base\DynamicModel;
-use yii\helpers\ArrayHelper;
 
 /**
  * @group FontAwesomeInline
@@ -26,9 +25,9 @@ class ActiveFormTest extends tests {
 		ActiveForm::end();
 		$out = ob_get_clean();
 
-		$expected = <<<HTML
+		$expected = <<<html
 <div class="input-group"><div class="input-group-prepend"><div class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa-fw svg-inline--fa svg-inline--fa-w-14" aria-hidden="true" role="img"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z" fill="currentColor"/></svg></div></div><input type="text" id="dynamicmodel-test" class="form-control" name="DynamicModel[test]"></div>
-HTML;
+html;
 
 		$this->assertContains($expected, $out);
 	}
@@ -40,7 +39,7 @@ HTML;
 		$form = ActiveForm::begin();
 			echo $form->field($model, 'test', [
 				'icon' => [
-					'name' => 'github',
+					'name' => 'bandcamp',
 					'class' => 'yourClass',
 					'fill' => '#003865',
 					'direction' => 'append',
@@ -50,9 +49,9 @@ HTML;
 		ActiveForm::end();
 		$out = ob_get_clean();
 
-		$expected = <<<HTML
-<div class="input-group"><input type="text" id="dynamicmodel-test" class="form-control" name="DynamicModel[test]"><div class="input-group-append"><div class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="yourClass svg-inline--fa-fw svg-inline--fa svg-inline--fa-w-16" aria-hidden="true" role="img"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" fill="#003865"/></svg></div></div></div>
-HTML;
+		$expected = <<<html
+<div class="input-group"><input type="text" id="dynamicmodel-test" class="form-control" name="DynamicModel[test]"><div class="input-group-append"><div class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="yourClass svg-inline--fa-fw svg-inline--fa svg-inline--fa-w-16" aria-hidden="true" role="img"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm48.2 326.1h-181L199.9 178h181l-84.7 156.1z" fill="#003865"/></svg></div></div></div>
+html;
 
 		$this->assertContains($expected, $out);
 	}
