1c966188244aa3f5af03088f78bd4a58b9011c6b
Thoulah <johan@mr42.me>
2019-07-18 21:51:55 +0200

    Clarified some options

--- docs/options.md
+++ docs/options.md
@@ -14,20 +14,26 @@
 
 *   `fixedWidth` bool. Set to `true` to have a fixed width icon.
 
-*   `height` int. The height of the icon. This will override height and width classes.
+*   `height` int. The height of the icon. This will dismiss the automatic height
+    and width classes. If `height` is given without `width`, the latter
+    will be calculated from the SVG size.
 
-*   `id` string. ID for the SVG tag.
+*   `id` string. Id for the SVG tag.
 
 *   `prefix` string. CSS class name, requires custom CSS if changed.
 
 *   `title` string. Sets a title to the SVG output.
 
-*   `width` int. The width of the icon. This will override height and width classes.
+*   `width` int. The width of the icon. This will dismiss the automatic height
+    and width classes. If `width` is given without `height`, the latter
+    will be calculated from the SVG size.
 
 ### ActiveForm Specific Options
 
-*   `append` bool. Whether to prepend or append the `input-group`.
+*   `append` bool. Whether to prepend or append the `input-group`. This will
+    change both tag order and applied class.
 
 *   `fixedWidth` bool. Set to `false` to have variable width icons.
 
-*   `groupSize` string. Set to `sm` for small or `lg` for large.
+*   `groupSize` string. Set to `sm` for small or `lg` for large. Defaults to
+    `md` or medium.
--- src/config/Options.php
+++ src/config/Options.php
@@ -12,7 +12,8 @@ use yii\helpers\ArrayHelper;
  */
 class Options extends BaseConfig
 {
-    /** @var bool ActiveForm specific option. Whether to prepend or append the `input-group` */
+    /** @var bool ActiveForm specific option. Whether to prepend or append the `input-group`
+     * This will change both tag order and applied class. */
     public $append = false;
 
     /** @var string Additional custom classes */
@@ -27,28 +28,41 @@ class Options extends BaseConfig
     /** @var bool Whether or not to have fixed width icons */
     public $fixedWidth;
 
-    /** @var string ActiveForm specific option. Set to `sm` for small or `lg` for large */
+    /**
+     * @var string ActiveForm specific option. Set to `sm` for small or `lg` for large
+     * Defaults to `md` or medium.
+     * */
     public $groupSize;
 
-    /** @var int The height of the icon. This will override height and width classes. */
+    /** @var int The height of the icon. This will dismiss the automatic height
+     * and width classes. If `height` is given without `width`, the latter
+     * will be calculated from the SVG size. */
     public $height;
 
-    /** @var string ID for the SVG tag */
+    /** @var string Id for the SVG tag */
     public $id;
 
-    /** @var string Name of the icon, picked from [Icons](https://fontawesome.com/icons) */
+    /**
+     * @var string Name of the icon
+     * @see https://fontawesome.com/icons
+     */
     public $name;
 
     /** @var string CSS class name, requires custom CSS if changed */
     public $prefix;
 
-    /** @var string Style of the icon, must match `name` */
+    /**
+     * @var string Style of the icon, must match `name`
+     * @see https://fontawesome.com/icons
+     */
     public $style;
 
     /** @var string Sets a title to the SVG output */
     public $title;
 
-    /** @var int The width of the icon. This will override height and width classes. */
+    /** @var int The width of the icon. This will dismiss the automatic height
+     * and width classes. If `height` is given without `width`, the latter
+     * will be calculated from the SVG size. */
     public $width;
 
     /**
